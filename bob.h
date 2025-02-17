a0d41ab305cdc9e868915fffb224f01befd04bc6
Markian Pirko <markianpirko456@gmail.com>
2025-02-17 15:30:07 +0200

    like

---
+++ bob.h
@@ -0,0 +1,7 @@
+#pragma once
+class bob
+{
+public:
+	int bib;
+};
+
--- test2.cpp
+++ test2.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include "bob.h"
 int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
+
+    bob a;
+    a.bib = 1;
+    std::cout << a.bib;
 }
